5808e34bfb6fc9b2affd5b3a868febfbb40e0a5c
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2025-01-08 16:32:30 +0100

    ADD show route

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -17,6 +17,26 @@ function index(req, res) {
 
 function show(req, res) {
     console.log("Show")
+
+    const id = req.params.id
+
+    let sql = `SELECT * FROM movies WHERE id = ?`
+
+    conn.query(sql, [id], (err, result) => {
+        if (err)
+            return res.status(500).json({
+                message: err.message
+            })
+        else if (res.length === 0)
+            return res.status(404).json({
+                error: "Not found",
+                message: "Movie not found"
+            })
+
+        const movie = result[0]
+
+        res.json(movie)
+    })
 }
 
 module.exports = { index, show }
