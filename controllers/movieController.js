5af6bc92989bfff41503e899c5621469bfbf8c19
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2025-01-08 16:28:31 +0100

    ADD index route

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,6 +2,17 @@ const conn = require("../data/db")
 
 function index(req, res) {
     console.log("Index")
+
+    let sql = `SELECT * FROM movies`
+
+    conn.query(sql, (err, movies) => {
+        if (err)
+            return res.status(500).json({
+                message: err.message
+            })
+
+        res.json(movies)
+    })
 }
 
 function show(req, res) {
